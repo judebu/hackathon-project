68116074a679cf58be3ca3958e27ce9da836ceba
KaziHossain15 <kazhoss1@gmail.com>
2025-10-26 11:31:18 -0400

    rebuilt

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  // 👇 This ensures assets load correctly on GitHub Pages
+  // Replace 'Terrier-Taste' if your repo name changes
   base: '/Terrier-Taste/',
 })
